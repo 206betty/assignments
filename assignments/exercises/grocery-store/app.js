db5f6557bcef41473b1e54c6cf1e7b3eb1f3e52a
V School Student <206betty@gmail.com>
2018-04-19 08:00:25 -0600

    update

---
+++ assignments/exercises/grocery-store/app.js
@@ -0,0 +1,9 @@
+var shopper = {
+    
+    firstName: "Sally",
+    lastName: "Little",
+    memberNumber: "456",
+    shoppingCart: ["eggs", "popsicles", "carrots"]
+}
+
+console.log(shopper.shoppingCart[1])
