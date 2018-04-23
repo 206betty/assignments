ab283f39b5dc212f5da2298cb11925b0bab3ebff
V School Student <206betty@gmail.com>
2018-04-23 08:48:56 -0600

    update

--- assignments/exercises/practice/loopolympics/app.js
+++ assignments/exercises/practice/loopolympics/app.js
@@ -8,8 +8,8 @@ for (var i = 9; i>= 0; i--) {
 
 var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
 
-for (var i = 0; i < fruit.length; 1+2) {
-    console.log(fruit[i])
+for (var i = 0; i < fruit.length; i++) {
+    console.log(fruit[i]);
 }
 
 var people = ["Cody", "Betty", "Marit", "Dave"]
@@ -17,3 +17,27 @@ var people = ["Cody", "Betty", "Marit", "Dave"]
 for (var i = 0; i < people.length; i++) {
     console.log(people[i])
 }
+
+
+var peopleArray = [  
+    {
+      name: "Harrison Ford",
+      occupation: "Actor"
+    },
+    {
+      name: "Justin Bieber",
+      occupation: "Singer"
+    },
+    {
+      name: "Vladimir Putin",
+      occupation: "Politician"
+    },
+    {
+      name: "Oprah",
+      occupation: "Entertainer"
+    }
+  ]
+
+  for (var i = 0; i = 0, 2; i++) {
+      console.log(peopleArray.length[i])
+  }
