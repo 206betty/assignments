2b809cf49dc2264d8f9791bbe86ab1adeda7076e
V School Student <206betty@gmail.com>
2018-04-29 22:48:11 -0600

    update

---
+++ assignments/exercises/practice/loopsex/app.js
@@ -0,0 +1,43 @@
+// // function pajamas(str) {
+//     for (var i = 0; i < str.length; i++){
+//         console.log(str[i])
+//     }
+// }
+// // pajamas("chocolate cake")
+
+// function ice(str, char) {
+//     for (var i = 0; i < str.length; i++) {
+//         if (char === str[i]){
+//             return i; 
+//         }
+//         }
+//             return "char is not found";
+//     }
+// console.log(ice("crushed","x"));
+
+// var array = ["12", "37", "70"];
+// var x = "42";
+
+
+// function find(arr, num) {
+//     for (var i = 0; i < arr.length; i++) {
+//         if (arr[i] === num) {
+//             return "42 is found";
+//         }
+// }
+//         return "42 not found";
+// }
+
+// console.log(find(array, x))
+
+var numbers = [17, 24, 34, 30, 12, 87, 62];
+var smallest = 0;
+
+function indexOfSmallest(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < arr[lowest] = i; {  
+        }
+        return lowest;
+}
+}
+console.log(indexOfSmallest(numbers, smallest))
